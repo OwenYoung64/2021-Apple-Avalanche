c80b5b8edaae57da6e5d78b9fe4f46c3cd0b6141
Owen Young <owen.young@vtsd.com>
2021-11-08 13:46:43 -0500

    commit 11/8/21

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #   a123_apple_1.py
 import turtle as trtl
-
+import random as rand
 #-----setup-----
 apple_image = "apple.gif" # Store the file name of your shape
 fontsetup = "Arial, 12"
@@ -10,16 +10,28 @@ wn.addshape(apple_image) # Make the screen aware of the new file
 wn.bgpic("background.gif")
 apple = trtl.Turtle()
 wn.tracer(False)
+screen_width = 400
+screen_height = 400
+letter_list = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 xoffset = -20
 yoffset = -47
 
 #-----functions-----
 
 # given a turtle, set that turtle to be shaped by the image file
-def draw_apple(active_apple):
+def reset_apple(active_apple):
+  length = len(letter_list)
+  if(length != 0):
+    index = rand.randint(0, length)
+    active_apple.goto(rand.randint(-screen_width/2, screen_width/2), rand.randint(-screen_height/2, screen_height/2))
+    draw_apple(active_apple, letter_list.pop(index))
+
+
+def draw_apple(active_apple, letter):
   active_apple.penup()
   active_apple.shape(apple_image)
-  draw_letter("A", active_apple)
+  active_apple.showturtle()
+  draw_letter(letter, active_apple)
   wn.update()
 
 def drop_apple():
@@ -29,6 +41,7 @@ def drop_apple():
   apple.hideturtle()
   apple.clear()
   wn.tracer(False)
+  reset_apple(apple)
 
 def draw_letter(letter, active_apple):
   active_apple.color("white")
@@ -38,7 +51,7 @@ def draw_letter(letter, active_apple):
   active_apple.setpos(remember_pos)
 
 #-----function calls-----
-draw_apple(apple)
+draw_apple(apple, "G")
 wn.onkeypress(drop_apple, "a")
 wn.listen()
 wn.mainloop()
